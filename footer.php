c562de1fc0d6b7f644f2ca5ddc2224b7a5d36711
Jahanban <jahanban@gmail.com>
2020-10-19 20:40:23 -0400

    Header and footer styled, fonts pending. Working on Blog archive

---
+++ footer.php
@@ -0,0 +1,25 @@
+</main>
+<footer>
+  
+
+<h2 class="title-footer">Rocket Vision</h2>
+
+<div class="social-icons">
+    <p>Follow us on:</p>
+    <a href=""><i class="fab fa-facebook-f"></i></a>
+    <a href=""><i class="fab fa-instagram"></i></a>
+    <a href=""> <i class="fab fa-twitter"></i></a>
+    
+    
+    
+   
+</div>
+
+
+</footer>
+
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ page-home.php
@@ -0,0 +1,17 @@
+<?php /* Template Name: Home */ ?>
+
+<?php get_header(); ?>
+
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+    <?php the_title(); ?>
+    <?php the_post_thumbnail(); ?>
+
+    
+
+  <?php endwhile; ?>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
